32223f4f225cc597499f0231a0aea44820c229d3
swhachey <sw.hachey@gmail.com>
2021-02-22 20:00:56 -0700

    added some routes and my server listener

--- server.js
+++ server.js
@@ -8,3 +8,23 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// ROUTING
+// GET Home HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
+
+// GET notes HTML
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './notes.html'));
+});
+
+// GET all notes in json
+app.get('/api/notes', (req, res) => {
+  res.json(noteJSON);
+});
+
+
+//Server Listener
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
