371181cd2d216fa21a5a4d3590bc58bad125b83b
swhachey <sw.hachey@gmail.com>
2021-02-22 20:40:40 -0700

    developed server.js file to route the new notes with post and get

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ const PORT = 8080;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+// Needed to display html in local browser
+app.use(express.static('./'));
+
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ROUTING
 // GET Home HTML
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // GET notes HTML
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // GET all notes in json
@@ -25,6 +30,12 @@ app.get('/api/notes', (req, res) => {
   res.json(noteJSON);
 });
 
+app.post('/api/notes', (req, res) => {
+const lastId = noteJSON.length ? Math.max(...(noteJSON.map(note => note.id))) : 0;
+const id = lastId + 1;
+noteJSON.push( { id, ...req.body} );
+res.json(noteJSON.slice(-1));
+});
 
 //Server Listener
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
