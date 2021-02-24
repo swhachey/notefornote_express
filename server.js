367076270b46bd261883245b49a5f4b12f573e2c
swhachey <sw.hachey@gmail.com>
2021-02-24 14:44:29 -0700

    added dynamic port for server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const noteJSON = require('./db/db.json');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
